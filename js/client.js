851a2242da1a497ee394d2b2e4c265937e7c1833
Shivangi Chandra <57565734+shivangi2174@users.noreply.github.com>
2023-03-16 12:40:45 +0530

    updated client.js

--- js/client.js
+++ js/client.js
@@ -1,4 +1,6 @@
-const socket = io('http://localhost:8000');
+//const socket = io('http://localhost:8000');
+
+const socket = socketClient('https://shivangi2174.github.io/RChat/:8000')
 
 const form = document.getElementById('send-container');
 const messageInput = document.getElementById('messageInpt');
